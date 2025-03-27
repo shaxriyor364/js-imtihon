9e621c5dc2a44148e9fcf513b3d5ad996b1d1758
unknown <your-email@example.com>
2025-03-27 16:15:39 +0500

    sdfvgsrdrsrrs

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    stars.forEach((s, i) => {
+      if (i <= index) {
+        s.classList.add("active");
+      } else {
+        s.classList.remove("active");
+      }
+    });
+  });
+});
